71133389dcab0d61a8f5f1f5950fa0b43132f5fb
juliankraft <kraftjulian@gmail.com>
2024-05-05 19:33:06 +0200

    rgb pictures

--- create_gif.py
+++
@@ -1,8 +0,0 @@
-import glob
-from PIL import Image
-
-def make_gif(frame_folder, gifname):
-    frames = [Image.open(image) for image in glob.glob(f"{frame_folder}/*.png")]
-    frame_one = frames[0]
-    frame_one.save(gifname, format="GIF", append_images=frames,
-               save_all=True, duration=1000, loop=0)
